95bafc9b1da97fd15273833418e324dbda21dd14
Antoine-NVE <antoinenavette@gmail.com>
2025-04-28 14:24:03 +0200

    feature: update and remove

--- backend/src/controllers/transaction.ts
+++ backend/src/controllers/transaction.ts
@@ -111,8 +111,111 @@ const getById = async (req: Request<{ id: string }>, res: Response) => {
     }
 };
 
+const update = async (req: Request<{ id: string }, {}, TransactionBody>, res: Response) => {
+    const { id } = req.params;
+    const { month, income, fixed, name, value } = req.body;
+
+    try {
+        const transaction = await TransactionModel.findByIdAndUpdate(
+            id,
+            {
+                month,
+                income,
+                fixed,
+                name,
+                value,
+            },
+            { new: true }
+        );
+
+        if (!transaction) {
+            res.status(404).json({
+                message: 'Transaction not found',
+                data: null,
+                errors: null,
+            });
+        }
+
+        res.status(200).json({
+            message: 'Transaction updated successfully',
+            data: {
+                transaction,
+            },
+            errors: null,
+        });
+    } catch (error: unknown) {
+        if (error instanceof MongooseError.ValidationError) {
+            const errors: Record<string, string> = {};
+
+            for (const [key, err] of Object.entries(error.errors)) {
+                errors[key] = err.message;
+            }
+
+            res.status(400).json({
+                message: 'Validation error',
+                data: null,
+                errors,
+            });
+        }
+
+        if (error instanceof MongooseError.CastError) {
+            res.status(400).json({
+                message: 'Invalid transaction ID',
+                data: null,
+                errors: null,
+            });
+        }
+
+        res.status(500).json({
+            message: 'Internal server error',
+            data: null,
+            errors: null,
+        });
+    }
+};
+
+const remove = async (req: Request<{ id: string }>, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const transaction = await TransactionModel.findByIdAndDelete(id);
+
+        if (!transaction) {
+            res.status(404).json({
+                message: 'Transaction not found',
+                data: null,
+                errors: null,
+            });
+        }
+
+        res.status(200).json({
+            message: 'Transaction deleted successfully',
+            data: {
+                transaction,
+            },
+            errors: null,
+        });
+    } catch (error: unknown) {
+        if (error instanceof MongooseError.CastError) {
+            res.status(400).json({
+                message: 'Invalid transaction ID',
+                data: null,
+                errors: null,
+            });
+        }
+
+        res.status(500).json({
+            message: 'Internal server error',
+            data: null,
+            errors: null,
+        });
+    }
+};
+
 export default {
     create,
     getAll,
     getById,
+    update,
+    remove,
 };
--- backend/src/routes/transaction.ts
+++ backend/src/routes/transaction.ts
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/', TransactionController.create);
 router.get('/', TransactionController.getAll);
 router.get('/:id', TransactionController.getById);
+router.put('/:id', TransactionController.update);
+router.delete('/:id', TransactionController.remove);
 
 export default router;
