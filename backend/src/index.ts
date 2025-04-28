73bfd2ad8f42b08bbf7abf159bf81eed8b14265f
Antoine-NVE <antoinenavette@gmail.com>
2025-04-28 12:14:29 +0200

    refactor(index.ts): order

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,21 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config();
-
 import TransactionModel from './models/Transaction';
 
+dotenv.config();
+
 const app = express();
+
+(async () => {
+    try {
+        const mongoUri = process.env.MONGO_URI || 'mongodb://ledge-database:27017/ledge';
+        await mongoose.connect(mongoUri);
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+    }
+})();
 
 app.get('/', async (req: Request, res: Response) => {
     const transaction = new TransactionModel({
@@ -25,15 +35,6 @@ app.get('/', async (req: Request, res: Response) => {
 
     res.send('Hello World from Backend!');
 });
-
-(async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI || 'mongodb://ledge-database:27017/ledge');
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Error connecting to MongoDB:', err);
-    }
-})();
 
 const port = process.env.EXPRESS_PORT || 3000;
 app.listen(port, () => {
