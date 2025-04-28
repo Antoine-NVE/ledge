a1833a7b90c79a425f30ae2c03c519d613dc905c
Antoine-NVE <antoinenavette@gmail.com>
2025-04-28 14:16:56 +0200

    feature: get transactions

--- backend/src/controllers/transaction.ts
+++ backend/src/controllers/transaction.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, RequestHandler, Response } from 'express';
 import TransactionModel from '../models/Transaction';
 import { Error as MongooseError } from 'mongoose';
 
@@ -43,16 +43,76 @@ const create = async (req: Request<{}, {}, TransactionBody>, res: Response) => {
                 data: null,
                 errors,
             });
-        } else {
-            res.status(500).json({
-                message: 'Internal server error',
+        }
+
+        res.status(500).json({
+            message: 'Internal server error',
+            data: null,
+            errors: null,
+        });
+    }
+};
+
+const getAll = async (req: Request, res: Response) => {
+    try {
+        const transactions = await TransactionModel.find();
+
+        res.status(200).json({
+            message: 'Transactions retrieved successfully',
+            data: {
+                transactions,
+            },
+            errors: null,
+        });
+    } catch (error: unknown) {
+        res.status(500).json({
+            message: 'Internal server error',
+            data: null,
+            errors: null,
+        });
+    }
+};
+
+const getById = async (req: Request<{ id: string }>, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const transaction = await TransactionModel.findById(id);
+
+        if (!transaction) {
+            res.status(404).json({
+                message: 'Transaction not found',
                 data: null,
                 errors: null,
             });
         }
+
+        res.status(200).json({
+            message: 'Transaction retrieved successfully',
+            data: {
+                transaction,
+            },
+            errors: null,
+        });
+    } catch (error: unknown) {
+        if (error instanceof MongooseError.CastError) {
+            res.status(400).json({
+                message: 'Invalid transaction ID',
+                data: null,
+                errors: null,
+            });
+        }
+
+        res.status(500).json({
+            message: 'Internal server error',
+            data: null,
+            errors: null,
+        });
     }
 };
 
 export default {
     create,
+    getAll,
+    getById,
 };
--- backend/src/routes/transaction.ts
+++ backend/src/routes/transaction.ts
@@ -4,5 +4,7 @@ import TransactionController from '../controllers/transaction';
 const router = express.Router();
 
 router.post('/', TransactionController.create);
+router.get('/', TransactionController.getAll);
+router.get('/:id', TransactionController.getById);
 
 export default router;
