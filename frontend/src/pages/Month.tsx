4addf1d4bf95e3063a36cff1f1b3d87f196fbbc2
Antoine-NVE <antoinenavette@gmail.com>
2025-04-29 20:06:36 +0200

    feature: colors on total

--- frontend/src/pages/Month.tsx
+++ frontend/src/pages/Month.tsx
@@ -27,7 +27,12 @@ const Month = () => {
 
             <div className="w-full max-w-5xl bg-white shadow-md rounded-lg p-4 mb-6 text-center">
                 <h3 className="text-xl font-semibold text-gray-800">Solde total</h3>
-                <p className="text-2xl font-bold text-gray-900">{total} €</p>
+                <p
+                    className={`text-2xl font-bold ${
+                        total > 0 ? 'text-green-900' : total < 0 ? 'text-red-900' : 'text-black'
+                    }`}>
+                    {total} €
+                </p>
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-5xl">
