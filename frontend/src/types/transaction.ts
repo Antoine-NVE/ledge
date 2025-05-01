bee7d412ae9f5ffedd2ce2a2db3f5df2fbd7aa2b
Antoine-NVE <antoinenavette@gmail.com>
2025-05-01 17:21:37 +0200

    chore: separate types for transactions and new transactions

--- frontend/src/types/transaction.ts
+++ frontend/src/types/transaction.ts
@@ -1,10 +1,13 @@
-export interface Transaction {
-    _id: string;
+export interface NewTransaction {
     month: string;
     isIncome: boolean;
     isFixed: boolean;
     name: string;
     value: number;
+}
+
+export interface Transaction extends NewTransaction {
+    _id: string;
     createdAt: string;
     updatedAt: string;
 }
