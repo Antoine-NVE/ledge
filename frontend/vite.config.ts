d18e47f5ad1fdda1197f91fae40d8f341f479e49
Antoine-NVE <antoinenavette@gmail.com>
2025-05-04 13:38:35 +0200

    chore: allowed hosts for Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    const allowedHosts = env.VITE_ALLOWED_HOSTS ? env.VITE_ALLOWED_HOSTS.split(',').map((host) => host.trim()) : [];
+
+    return {
+        plugins: [react(), tailwindcss()],
+        server: {
+            allowedHosts,
+        },
+    };
 });
