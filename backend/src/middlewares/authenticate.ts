66b5231fb5b5c246ba45a50f4f616cba46ce16a8
Antoine-NVE <antoinenavette@gmail.com>
2025-05-18 15:28:28 +0200

    fix: handle null access token

--- backend/src/middlewares/authenticate.ts
+++ backend/src/middlewares/authenticate.ts
@@ -1,7 +1,7 @@
 import { verifyAccessToken } from '../services/auth';
 
 export const authenticate = (req: any, res: any, next: any) => {
-    const access_token = req.cookies.access_token;
+    const access_token = req.cookies?.access_token;
 
     if (!access_token) {
         return res.status(401).json({
