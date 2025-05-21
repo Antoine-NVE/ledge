1475530af34f22b0573bb3fbb3d6f364ca39058b
Antoine-NVE <antoinenavette@gmail.com>
2025-05-21 13:04:17 +0200

    feat: add CORS rules

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    }),
+);
 
 (async () => {
     try {
