325ddf4828539eeb1e58907a2d23318fed853cb7
Antoine-NVE <antoinenavette@gmail.com>
2025-05-25 17:49:34 +0200

    style: delete useless brackets

--- frontend/src/hooks/useTransaction.ts
+++ frontend/src/hooks/useTransaction.ts
@@ -3,9 +3,8 @@ import TransactionContext from '../contexts/TransactionContext';
 
 const useTransaction = () => {
     const context = useContext(TransactionContext);
-    if (!context) {
-        throw new Error('useTransactions must be used within a TransactionProvider');
-    }
+    if (!context) throw new Error('useTransactions must be used within a TransactionProvider');
+
     return context;
 };
 
