e9fdd1bb606f29056b09730bb8af9308802620fa
Antoine-NVE <antoinenavette@gmail.com>
2025-05-26 16:42:07 +0200

    feat: create registration API call

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -3,6 +3,38 @@ import { User } from '../types/user';
 
 const API_URL = import.meta.env.VITE_API_URL + '/auth';
 
+export const register = async (
+    email: string,
+    password: string
+): Promise<[ApiResponse<{ user: User } | null, null>, Response | null]> => {
+    try {
+        const response = await fetch(API_URL + '/register', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        
+        // Can be any status code, including 200, 401, or 500
+        // We will handle this in the component
+        const result: ApiResponse<{ user: User } | null, null> = await response.json();
+        return [result, response];
+    } catch (error: unknown) {
+        console.error(error);
+
+        return [
+            {
+                message: 'An error occurred while registering',
+                data: null,
+                errors: null,
+            },
+            null, // In this case, we didn't receive a response from the server
+        ];
+    }
+};
+
 export const login = async (
     email: string,
     password: string
