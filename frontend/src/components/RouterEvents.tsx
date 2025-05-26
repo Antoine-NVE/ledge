6d9b3e52bec7c267676a80b9497b9da783a319ad
Antoine-NVE <antoinenavette@gmail.com>
2025-05-26 17:42:01 +0200

    fix: set user to null when redirecting from 401

--- frontend/src/components/RouterEvents.tsx
+++ frontend/src/components/RouterEvents.tsx
@@ -1,10 +1,14 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useUser from '../hooks/useUser';
 
 const RouterEvents = () => {
+    const { setUser } = useUser();
     const navigate = useNavigate();
 
     useEffect(() => {
+        setUser(null);
+
         const handler = () => navigate('/login');
         window.addEventListener('unauthorized', handler);
         return () => window.removeEventListener('unauthorized', handler);
