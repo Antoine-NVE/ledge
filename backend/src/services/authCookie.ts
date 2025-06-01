0f5539f96eff7647f548de1c291a79c6a03463b2
Antoine-NVE <antoinenavette@gmail.com>
2025-06-01 15:51:00 +0200

    feat: add functions to set and clear refresh token cookie

--- backend/src/services/authCookie.ts
+++ backend/src/services/authCookie.ts
@@ -16,3 +16,20 @@ export const clearAccessToken = (res: Response) => {
         sameSite: 'strict',
     });
 };
+
+export const setRefreshTokenCookie = (res: Response, token: string) => {
+    res.cookie('refresh_token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 604800000, // 7 days
+    });
+};
+
+export const clearRefreshToken = (res: Response) => {
+    res.clearCookie('refresh_token', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+    });
+};
