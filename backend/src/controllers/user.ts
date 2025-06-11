1eaf6db8108f6d009ba618ff636f3f55a6ca87f2
Antoine-NVE <antoinenavette@gmail.com>
2025-06-11 17:31:29 +0200

    feat: verification email route

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import nodemailer from 'nodemailer';
 
 export const getUser = async (req: Request, res: Response) => {
     try {
@@ -9,6 +10,52 @@ export const getUser = async (req: Request, res: Response) => {
             data: {
                 user,
             },
+            errors: null,
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: 'Internal server error',
+            data: null,
+            errors: null,
+        });
+    }
+};
+
+export const sendVerificationEmail = async (req: Request, res: Response) => {
+    try {
+        const user = req.user!;
+
+        const transporter = nodemailer.createTransport({
+            host: 'smtp',
+            port: 1025,
+            secure: false,
+        });
+
+        const mailOptions = {
+            from: '"Ledge" <no-reply@ledge.com>',
+            to: user.email,
+            subject: 'Please verify your email address',
+            html: 'Click here to verify your email address: <a href="http://localhost:3000/verify-email?token=your-verification-token">Verify Email</a>',
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(error);
+
+                res.status(500).json({
+                    message: 'Failed to send verification email',
+                    data: null,
+                    errors: error.message,
+                });
+                return;
+            }
+        });
+
+        res.status(200).json({
+            message: 'Verification email sent successfully',
+            data: null,
             errors: null,
         });
     } catch (error) {
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getUser } from '../controllers/user';
+import { getUser, sendVerificationEmail } from '../controllers/user';
 import authenticate from '../middlewares/authenticate';
 
 const router = express.Router();
 
+router.post('/send-verification-email', authenticate, sendVerificationEmail);
 router.get('/me', authenticate, getUser);
 
 export default router;
