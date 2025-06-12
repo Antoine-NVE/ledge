b46e0bd831d4bec0569002c6fa032003fdbd168f
Antoine-NVE <antoinenavette@gmail.com>
2025-06-12 22:06:51 +0200

    feat: hide button to send verification email if already verified

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { sendVerificationEmail } from '../api/user';
+import useUser from '../hooks/useUser';
 
 const Profile = () => {
+    const { user } = useUser();
+
     const [loading, setLoading] = useState(false);
     const [success, setSuccess] = useState<boolean | null>(null);
     const [message, setMessage] = useState<string | null>(null);
@@ -28,12 +31,14 @@ const Profile = () => {
         <div className="flex flex-col flex-1 items-center justify-center p-4">
             <h1 className="text-4xl font-bold mb-4">Profile page</h1>
             <p className="text-lg mb-6">This is the profile page.</p>
-            <button
-                className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition cursor-pointer disabled:opacity-50"
-                onClick={handleSendVerificationEmail}
-                disabled={loading}>
-                {loading ? 'Sending...' : 'Send verification email'}
-            </button>
+            {!user?.isEmailVerified && (
+                <button
+                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition cursor-pointer disabled:opacity-50"
+                    onClick={handleSendVerificationEmail}
+                    disabled={loading}>
+                    {loading ? 'Sending...' : 'Send verification email'}
+                </button>
+            )}
             {message && <div className={`mt-4 ${success ? 'text-green-600' : 'text-red-600'}`}>{message}</div>}
         </div>
     );
