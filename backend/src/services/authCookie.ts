39059ca13c3828fd93a072680b0f4e324137a716
Antoine-NVE <antoinenavette@gmail.com>
2025-08-16 16:28:47 +0200

    refactor: clean cookies options

--- backend/src/services/authCookie.ts
+++ backend/src/services/authCookie.ts
@@ -1,52 +1,54 @@
 import { Response } from 'express';
 
+type CookieOptions = { httpOnly: boolean; secure: boolean; sameSite: 'strict' | 'lax' | 'none' };
+
+const cookieBaseOptions: CookieOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+};
+
+const rememberMeCookieOptions: CookieOptions = {
+    httpOnly: false,
+    secure: true,
+    sameSite: 'strict',
+};
+
 export const setAccessTokenCookie = (res: Response, token: string, rememberMe: boolean) => {
     res.cookie('access_token', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
+        ...cookieBaseOptions,
         ...(rememberMe ? { maxAge: 3600000 } : {}), // 1 hour
     });
 };
 
 export const clearAccessToken = (res: Response) => {
     res.clearCookie('access_token', {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
+        ...cookieBaseOptions,
     });
 };
 
 export const setRefreshTokenCookie = (res: Response, token: string, rememberMe: boolean) => {
     res.cookie('refresh_token', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
+        ...cookieBaseOptions,
         ...(rememberMe ? { maxAge: 604800000 } : {}), // 7 days
     });
 };
 
 export const clearRefreshToken = (res: Response) => {
     res.clearCookie('refresh_token', {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
+        ...cookieBaseOptions,
     });
 };
 
 export const setRememberMeCookie = (res: Response, rememberMe: boolean) => {
     res.cookie('remember_me', rememberMe, {
-        httpOnly: false,
-        secure: true,
-        sameSite: 'strict',
+        ...rememberMeCookieOptions,
         ...(rememberMe ? { maxAge: 604800000 } : {}), // 7 days
     });
 };
 
 export const clearRememberMeCookie = (res: Response) => {
     res.clearCookie('remember_me', {
-        httpOnly: false,
-        secure: true,
-        sameSite: 'strict',
+        ...rememberMeCookieOptions,
     });
 };
