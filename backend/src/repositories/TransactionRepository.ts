7e566bb63e828b9ccc810556927caa1544528f32
Antoine-NVE <antoinenavette@gmail.com>
2025-09-23 18:54:50 +0200

    feat: transaction repository

---
+++ backend/src/repositories/TransactionRepository.ts
@@ -0,0 +1,28 @@
+import { Model, Types } from 'mongoose';
+import { Transaction, TransactionDocument } from '../models/Transaction';
+
+export default class TransactionRepository {
+    constructor(private transactionModel: Model<TransactionDocument>) {}
+
+    async create(data: Partial<Transaction>): Promise<TransactionDocument> {
+        // We do not use transactionModel.create to ensure that pre-save hooks are executed
+        const transaction = new this.transactionModel(data);
+        return await transaction.save();
+    }
+
+    async findById(id: Types.ObjectId): Promise<TransactionDocument | null> {
+        return await this.transactionModel.findById(id);
+    }
+
+    async update(id: Types.ObjectId, data: Partial<Transaction>): Promise<TransactionDocument | null> {
+        // We do not use findByIdAndUpdate to ensure that pre-save hooks are executed
+        const transaction = await this.transactionModel.findById(id);
+        if (!transaction) return null;
+        Object.assign(transaction, data);
+        return await transaction.save();
+    }
+
+    async delete(id: Types.ObjectId): Promise<TransactionDocument | null> {
+        return await this.transactionModel.findByIdAndDelete(id);
+    }
+}
