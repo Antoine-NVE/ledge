7e524745d7a6bb1a88841cb8c111c159bb416d7c
Antoine-NVE <antoinenavette@gmail.com>
2025-09-23 18:38:34 +0200

    feat: user repository

---
+++ backend/src/repositories/UserRepository.ts
@@ -0,0 +1,26 @@
+import { Model, Types } from 'mongoose';
+import { User, UserDocument } from '../models/User';
+
+export default class UserRepository {
+    constructor(private userModel: Model<UserDocument>) {}
+
+    async create(data: Partial<User>): Promise<UserDocument> {
+        return await this.userModel.create(data);
+    }
+
+    async findById(id: Types.ObjectId): Promise<UserDocument | null> {
+        return await this.userModel.findById(id);
+    }
+
+    async update(id: Types.ObjectId, data: Partial<User>): Promise<UserDocument | null> {
+        // We do not use findByIdAndUpdate to ensure that pre-save hooks are executed
+        const user = await this.userModel.findById(id);
+        if (!user) return null;
+        Object.assign(user, data);
+        return await user.save();
+    }
+
+    async delete(id: Types.ObjectId): Promise<UserDocument | null> {
+        return await this.userModel.findByIdAndDelete(id);
+    }
+}
