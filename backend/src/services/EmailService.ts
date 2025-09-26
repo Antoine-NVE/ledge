f3698927ed22a0dc72ea3db5edcf458c1d3d0e8a
Antoine-NVE <antoinenavette@gmail.com>
2025-09-26 23:12:23 +0200

    refactor(email service): oop

--- backend/src/services/EmailService.ts
+++ backend/src/services/EmailService.ts
@@ -1,44 +1,30 @@
 import { Transporter, SentMessageInfo, createTransport } from 'nodemailer';
 
-export const createTransporter = (
-    host: string,
-    port: number,
-    secure: boolean,
-    auth: { user: string; pass: string },
-): Transporter => {
-    return createTransport({
+export class EmailService {
+    private transporter: Transporter;
+
+    constructor({
         host,
         port,
         secure,
         auth,
-    });
-};
-
-// Base function, only called in this service
-const sendEmail = async (
-    from: string,
-    to: string,
-    subject: string,
-    html: string,
-    transporter: Transporter,
-): Promise<SentMessageInfo | null> => {
-    try {
-        return await transporter.sendMail({ from, to, subject, html });
-    } catch (error) {
-        console.error(error);
-        return null;
+    }: {
+        host: string;
+        port: number;
+        secure: boolean;
+        auth: { user: string; pass: string };
+    }) {
+        this.transporter = createTransport({ host, port, secure, auth });
     }
-};
 
-export const sendEmailVerificationEmail = async (
-    from: string,
-    to: string,
-    transporter: Transporter,
-    frontendBaseUrl: string,
-    jwt: string,
-): Promise<SentMessageInfo | null> => {
-    const subject = 'Please verify your email address';
-    const html = `Click here to verify your email address: <a href="${frontendBaseUrl}/verify-email/${jwt}">verify email</a>. This link will expire in 1 hour.`;
+    private async sendEmail(from: string, to: string, subject: string, html: string): Promise<SentMessageInfo> {
+        return await this.transporter.sendMail({ from, to, subject, html });
+    }
 
-    return await sendEmail(from, to, subject, html, transporter);
-};
+    async sendEmailVerificationEmail(from: string, to: string, frontendBaseUrl: string, jwt: string) {
+        const subject = 'Please verify your email address';
+        const html = `Click here to verify your email address: <a href="${frontendBaseUrl}/verify-email/${jwt}">verify email</a>. This link will expire in 1 hour.`;
+
+        return this.sendEmail(from, to, subject, html);
+    }
+}
