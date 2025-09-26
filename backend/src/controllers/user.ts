05cfe16cd383048413485ecdb1cf09f93e776131
Antoine-NVE <antoinenavette@gmail.com>
2025-09-26 23:16:31 +0200

    refactor(user controller): rename function

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -3,26 +3,16 @@ import UserModel from '../models/User';
 import { createEmailVerificationJwt, verifyEmailVerificationJwt } from '../services/JwtService';
 import { createTransporter, sendEmailVerificationEmail } from '../services/EmailService';
 
-export const getUser = async (req: Request, res: Response) => {
-    try {
-        const user = req.user;
+export const me = async (req: Request, res: Response) => {
+    const user = req.user!;
 
-        res.status(200).json({
-            message: 'User retrieved successfully',
-            data: {
-                user,
-            },
-            errors: null,
-        });
-    } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            message: 'Internal server error',
-            data: null,
-            errors: null,
-        });
-    }
+    res.status(200).json({
+        message: 'User retrieved successfully',
+        data: {
+            user,
+        },
+        errors: null,
+    });
 };
 
 export const sendVerificationEmail = async (req: Request, res: Response) => {
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
-import { getUser, sendVerificationEmail, verifyEmail } from '../controllers/user';
+import { me, sendVerificationEmail, verifyEmail } from '../controllers/user';
 import authenticate from '../middlewares/authenticate';
 
 const router = express.Router();
 
 router.post('/send-verification-email', authenticate, sendVerificationEmail);
 router.get('/verify-email/:token', verifyEmail);
-router.get('/me', authenticate, getUser);
+router.get('/me', authenticate, me);
 
 export default router;
