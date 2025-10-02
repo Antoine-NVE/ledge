fcb6fd232eddadb0f6dc32d2188a7b6ba4dd6c44
Antoine-NVE <antoinenavette@gmail.com>
2025-10-02 18:43:47 +0200

    refactor(env): parsing functions

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,6 +2,19 @@ import dotenv from 'dotenv';
 import { envSchema } from '../schemas/configSchema';
 dotenv.config();
 
+const parseNumber = (value: string | undefined): number | undefined => {
+    if (value === undefined) return undefined;
+    const number = Number(value);
+    return isNaN(number) ? undefined : number;
+};
+
+const parseBoolean = (value: string | undefined): boolean | undefined => {
+    if (value === undefined) return undefined;
+    if (value === 'true') return true;
+    if (value === 'false') return false;
+    return undefined;
+};
+
 export const env = envSchema.parse({
     NODE_ENV: process.env.NODE_ENV,
 
@@ -12,13 +25,8 @@ export const env = envSchema.parse({
     ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
 
     SMTP_HOST: process.env.SMTP_HOST,
-    SMTP_PORT: Number(process.env.SMTP_PORT),
-    SMTP_SECURE:
-        process.env.SMTP_SECURE === 'true'
-            ? true
-            : process.env.SMTP_SECURE === 'false'
-              ? false
-              : undefined,
+    SMTP_PORT: parseNumber(process.env.SMTP_PORT),
+    SMTP_SECURE: parseBoolean(process.env.SMTP_SECURE),
     SMTP_USER: process.env.SMTP_USER,
     SMTP_PASS: process.env.SMTP_PASS,
     EMAIL_FROM: process.env.EMAIL_FROM,
