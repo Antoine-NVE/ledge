42cd380a701b87b866692a0bb28243d29b5ceff5
Antoine-NVE <antoinenavette@gmail.com>
2025-10-03 21:28:32 +0200

    feat(configSchema): strict mode

--- backend/src/schemas/configSchema.ts
+++ backend/src/schemas/configSchema.ts
@@ -1,18 +1,20 @@
 import z from 'zod';
 
-export const envSchema = z.object({
-    NODE_ENV: z.enum(['development', 'production']),
+export const envSchema = z
+    .object({
+        NODE_ENV: z.enum(['development', 'production']),
 
-    DATABASE_SERVICE: z.string().min(1),
+        DATABASE_SERVICE: z.string().min(1),
 
-    JWT_SECRET: z.string().min(1),
+        JWT_SECRET: z.string().min(1),
 
-    ALLOWED_ORIGINS: z.string().min(1),
+        ALLOWED_ORIGINS: z.string().min(1),
 
-    SMTP_HOST: z.string().min(1),
-    SMTP_PORT: z.number().min(1),
-    SMTP_SECURE: z.boolean(),
-    SMTP_USER: z.string(),
-    SMTP_PASS: z.string(),
-    EMAIL_FROM: z.string().min(1),
-});
+        SMTP_HOST: z.string().min(1),
+        SMTP_PORT: z.number().min(1),
+        SMTP_SECURE: z.boolean(),
+        SMTP_USER: z.string(),
+        SMTP_PASS: z.string(),
+        EMAIL_FROM: z.string().min(1),
+    })
+    .strict();
