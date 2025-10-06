c7e4e833e74ac71c7a6de2e0c6fb02e8cecf589d
Antoine-NVE <antoinenavette@gmail.com>
2025-10-06 21:19:07 +0200

    fix: type

--- backend/src/types/transaction.ts
+++ backend/src/types/transaction.ts
@@ -1,7 +1,8 @@
 import z from 'zod';
 import { refreshTokenSchema } from '../schemas/refreshTokenSchema';
 import { WithId } from 'mongodb';
+import { transactionSchema } from '../schemas/transactionSchema';
 
-export type TransactionData = z.infer<typeof refreshTokenSchema>;
+export type TransactionData = z.infer<typeof transactionSchema>;
 
 export type Transaction = WithId<TransactionData>;
