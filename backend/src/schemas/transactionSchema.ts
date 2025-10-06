11027ab70dfb24c027780e1f230bd4f51c7966d1
Antoine-NVE <antoinenavette@gmail.com>
2025-10-06 21:40:19 +0200

    feat(transaction): max 2 decimal places

--- backend/src/schemas/transactionSchema.ts
+++ backend/src/schemas/transactionSchema.ts
@@ -5,7 +5,14 @@ export const transactionCreateSchema = z
     .object({
         month: z.string().regex(/^\d{4}-(0[1-9]|1[0-2])$/),
         name: z.string().trim().min(1, 'Name is required').max(99, 'Name is too long'),
-        value: z.number().min(0.01, 'Value is too small').max(999999999.99, 'Value is too big'),
+        value: z
+            .number()
+            .min(0.01, 'Value is too small')
+            .max(999999999.99, 'Value is too big')
+            .refine(
+                (val) => Number.isInteger(val * 100),
+                'Value must have at most 2 decimal places',
+            ),
         isIncome: z.boolean(),
         isRecurring: z.boolean(),
     })
@@ -14,7 +21,14 @@ export const transactionCreateSchema = z
 export const transactionUpdateSchema = z
     .object({
         name: z.string().trim().min(1, 'Name is required').max(99, 'Name is too long'),
-        value: z.number().min(0.01, 'Value is too small').max(999999999.99, 'Value is too big'),
+        value: z
+            .number()
+            .min(0.01, 'Value is too small')
+            .max(999999999.99, 'Value is too big')
+            .refine(
+                (val) => Number.isInteger(val * 100),
+                'Value must have at most 2 decimal places',
+            ),
         isIncome: z.boolean(),
         isRecurring: z.boolean(),
     })
@@ -24,7 +38,11 @@ export const transactionSchema = z
     .object({
         month: z.string().regex(/^\d{4}-(0[1-9]|1[0-2])$/),
         name: z.string().trim().min(1).max(99),
-        value: z.number().min(0.01).max(999999999.99),
+        value: z
+            .number()
+            .min(0.01)
+            .max(999999999.99)
+            .refine((val) => Number.isInteger(val * 100)),
         isIncome: z.boolean(),
         isRecurring: z.boolean(),
         userId: z.custom<ObjectId>((val) => val instanceof ObjectId),
