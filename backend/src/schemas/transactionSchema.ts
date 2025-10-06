b062230281c711b289b52291d08f088871c27d4f
Antoine-NVE <antoinenavette@gmail.com>
2025-10-06 21:38:48 +0200

    style(transactionSchema): order

--- backend/src/schemas/transactionSchema.ts
+++ backend/src/schemas/transactionSchema.ts
@@ -4,19 +4,19 @@ import { ObjectId } from 'mongodb';
 export const transactionCreateSchema = z
     .object({
         month: z.string().regex(/^\d{4}-(0[1-9]|1[0-2])$/),
-        isIncome: z.boolean(),
-        isRecurring: z.boolean(),
         name: z.string().trim().min(1, 'Name is required').max(99, 'Name is too long'),
         value: z.number().min(0.01, 'Value is too small').max(999999999.99, 'Value is too big'),
+        isIncome: z.boolean(),
+        isRecurring: z.boolean(),
     })
     .strict();
 
 export const transactionUpdateSchema = z
     .object({
-        isIncome: z.boolean(),
-        isRecurring: z.boolean(),
         name: z.string().trim().min(1, 'Name is required').max(99, 'Name is too long'),
         value: z.number().min(0.01, 'Value is too small').max(999999999.99, 'Value is too big'),
+        isIncome: z.boolean(),
+        isRecurring: z.boolean(),
     })
     .strict();
 
