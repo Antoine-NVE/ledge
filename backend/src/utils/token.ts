804dc588ea1eb980b720081e0b193b0d10806146
Antoine-NVE <antoinenavette@gmail.com>
2025-10-09 19:19:18 +0200

    refactor: generate token function

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -1,5 +1,8 @@
 import crypto from 'crypto';
 
-export const generateToken = (length: number = 64): string => {
-    return crypto.randomBytes(length).toString('hex');
+export const generateToken = (charLength: number = 64): string => {
+    const length = Math.abs(Math.floor(charLength));
+    const byteLength = Math.ceil(length / 2);
+
+    return crypto.randomBytes(byteLength).toString('hex').slice(0, length);
 };
