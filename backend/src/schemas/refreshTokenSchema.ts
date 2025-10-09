8a1755c3980a75f16daa715f136a3e4dda587aae
Antoine-NVE <antoinenavette@gmail.com>
2025-10-09 19:21:37 +0200

    feat: force refresh token to 64 characters

--- backend/src/schemas/refreshTokenSchema.ts
+++ backend/src/schemas/refreshTokenSchema.ts
@@ -3,7 +3,7 @@ import { ObjectId } from 'mongodb';
 
 export const refreshTokenSchema = z
     .object({
-        token: z.string().trim().min(1),
+        token: z.string().length(64),
         expiresAt: z.date(),
         userId: z.custom<ObjectId>((val) => val instanceof ObjectId),
         createdAt: z.date(),
