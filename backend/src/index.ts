09577b5f42f32651e9f81fa95c2c023f328a6491
Antoine-NVE <antoinenavette@gmail.com>
2025-10-10 20:00:18 +0200

    feat(envSchema): transform origins to array

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,11 +16,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = env.ALLOWED_ORIGINS.split(',');
-
 app.use(
     cors({
-        origin: allowedOrigins,
+        origin: env.ALLOWED_ORIGINS,
         credentials: true,
     }),
 );
--- backend/src/schemas/configSchema.ts
+++ backend/src/schemas/configSchema.ts
@@ -8,7 +8,10 @@ export const envSchema = z
 
         JWT_SECRET: z.string(),
 
-        ALLOWED_ORIGINS: z.string(),
+        ALLOWED_ORIGINS: z
+            .string()
+            .transform((val) => val.split(','))
+            .pipe(z.array(z.string().url())),
 
         SMTP_HOST: z.string(),
         SMTP_PORT: z.number(),
