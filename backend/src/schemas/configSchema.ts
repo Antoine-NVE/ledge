70ecd43d9b1d113c4c6a5fdc46f1aceff2bd4c1f
Antoine-NVE <antoinenavette@gmail.com>
2025-10-10 19:54:12 +0200

    feat(envSchema): remove useless min

--- backend/src/schemas/configSchema.ts
+++ backend/src/schemas/configSchema.ts
@@ -4,17 +4,17 @@ export const envSchema = z
     .object({
         NODE_ENV: z.enum(['development', 'production']),
 
-        DATABASE_SERVICE: z.string().min(1),
+        DATABASE_SERVICE: z.string(),
 
-        JWT_SECRET: z.string().min(1),
+        JWT_SECRET: z.string(),
 
-        ALLOWED_ORIGINS: z.string().min(1),
+        ALLOWED_ORIGINS: z.string(),
 
-        SMTP_HOST: z.string().min(1),
-        SMTP_PORT: z.number().min(1),
+        SMTP_HOST: z.string(),
+        SMTP_PORT: z.number(),
         SMTP_SECURE: z.boolean(),
         SMTP_USER: z.string(),
         SMTP_PASS: z.string(),
-        EMAIL_FROM: z.string().min(1),
+        EMAIL_FROM: z.string(),
     })
     .strict();
