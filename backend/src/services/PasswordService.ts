aa26000b7c94e8fdddd8bd39f18682bd582dc595
Antoine-NVE <antoinenavette@gmail.com>
2025-10-14 21:46:07 +0200

    refactor: remove useless var

--- backend/src/services/PasswordService.ts
+++ backend/src/services/PasswordService.ts
@@ -1,10 +1,8 @@
 import bcrypt from 'bcrypt';
 
 export class PasswordService {
-    private readonly saltRounds = 10;
-
     hash = async (plain: string): Promise<string> => {
-        return await bcrypt.hash(plain, this.saltRounds);
+        return await bcrypt.hash(plain, 10);
     };
 
     compare = async (plain: string, hash: string): Promise<boolean> => {
