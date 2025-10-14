f6206b3477f8835e5a72379b739ff8e0f5b9d299
Antoine-NVE <antoinenavette@gmail.com>
2025-10-14 19:57:40 +0200

    refactor: schema utils tests

--- backend/src/__tests__/utils/schema.spec.ts
+++ backend/src/__tests__/utils/schema.spec.ts
@@ -6,48 +6,59 @@ import { InvalidDataError } from '../../errors/InternalServerError';
 describe('schema utils', () => {
     const schema = z.strictObject(
         {
-            field1: z.string().min(5, 'Error 1'),
-            field2: z.number().min(10, 'Error 2'),
+            field1: z.string().min(5, 'Error 1').startsWith('valid', 'Error 2'),
+            field2: z.number().min(10, 'Error 3'),
         },
         'Global error',
     );
 
+    const validData = {
+        field1: 'valid string',
+        field2: 15,
+    };
+
+    const validDataWithExtra = {
+        field1: 'valid string',
+        field2: 15,
+        field3: 'extra field',
+    };
+
+    const invalidData = {
+        field1: 'abc',
+        field2: 5,
+    };
+
+    const invalidDataWithExtra = {
+        field1: 'abc',
+        field2: 5,
+        field3: 'extra field',
+    };
+
     describe('formatError()', () => {
         it('should format ZodError correctly with formErrors and fieldErrors', () => {
-            const { error } = schema.safeParse({
-                field1: 'abc',
-                field2: 5,
-                field3: 'extra field',
-            });
+            const { error } = schema.safeParse(invalidDataWithExtra);
 
             const formatted = formatError(error!);
 
             expect(formatted).toEqual({
                 global: ['Global error'],
-                field1: ['Error 1'],
-                field2: ['Error 2'],
+                field1: ['Error 1', 'Error 2'],
+                field2: ['Error 3'],
             });
         });
 
         it('should format ZodError correctly with only fieldErrors', () => {
-            const { error } = schema.safeParse({
-                field1: 'abc',
-                field2: 5,
-            });
+            const { error } = schema.safeParse(invalidData);
 
             const formatted = formatError(error!);
             expect(formatted).toEqual({
-                field1: ['Error 1'],
-                field2: ['Error 2'],
+                field1: ['Error 1', 'Error 2'],
+                field2: ['Error 3'],
             });
         });
 
         it('should format ZodError correctly with only formErrors', () => {
-            const { error } = schema.safeParse({
-                field1: 'valid string',
-                field2: 15,
-                field3: 'extra field',
-            });
+            const { error } = schema.safeParse(validDataWithExtra);
 
             const formatted = formatError(error!);
             expect(formatted).toEqual({
@@ -58,44 +69,26 @@ describe('schema utils', () => {
 
     describe('parseSchema()', () => {
         it('should parse valid data correctly', () => {
-            const data = {
-                field1: 'valid string',
-                field2: 15,
-            };
-
-            const parsed = parseSchema(schema, data);
-            expect(parsed).toEqual(data);
+            const parsed = parseSchema(schema, validData);
+            expect(parsed).toEqual(validData);
         });
 
         it('should throw ValidationError for invalid data when isClientError is true', () => {
-            const data = {
-                field1: 'abc',
-                field2: 5,
-            };
-
-            expect(() => parseSchema(schema, data, true)).toThrow(
+            expect(() => parseSchema(schema, invalidData, true)).toThrow(
                 ValidationError,
             );
         });
 
         it('should throw InvalidDataError for invalid data when isClientError is false', () => {
-            const data = {
-                field1: 'abc',
-                field2: 5,
-            };
-
-            expect(() => parseSchema(schema, data, false)).toThrow(
+            expect(() => parseSchema(schema, invalidData, false)).toThrow(
                 InvalidDataError,
             );
         });
 
         it('should throw InvalidDataError for invalid data when isClientError is omitted', () => {
-            const data = {
-                field1: 'abc',
-                field2: 5,
-            };
-
-            expect(() => parseSchema(schema, data)).toThrow(InvalidDataError);
+            expect(() => parseSchema(schema, invalidData)).toThrow(
+                InvalidDataError,
+            );
         });
     });
 });
