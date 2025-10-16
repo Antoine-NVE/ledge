14873ac50aa51d875f7ec7be0ff39c001e0ae0ce
Antoine-NVE <antoinenavette@gmail.com>
2025-10-16 18:54:30 +0200

    test(schemas): unit tests

---
+++ backend/src/__tests__/schemas/config.spec.ts
@@ -0,0 +1,36 @@
+import { envSchema } from '../../schemas/config';
+
+describe('config schemas', () => {
+    describe('envSchema', () => {
+        const validData = {
+            NODE_ENV: 'development',
+            DATABASE_SERVICE: 'database',
+            JWT_SECRET: 'your_jwt_secret',
+            ALLOWED_ORIGINS: [
+                'http://localhost:5173',
+                'http://localhost:4173',
+                'http://localhost:8000',
+            ],
+            SMTP_HOST: 'smtp',
+            SMTP_PORT: 1025,
+            SMTP_SECURE: false,
+            SMTP_USER: '',
+            SMTP_PASS: '',
+            EMAIL_FROM: 'Ledge <no-reply@ledge.com>',
+        };
+
+        const validDataWithExtraKey = {
+            ...validData,
+            extraKey: 'extra value',
+        };
+
+        it('should accept valid data', () => {
+            const data = envSchema.parse(validData);
+            expect(data).toEqual(validData);
+        });
+
+        it('should refuse extra key', () => {
+            expect(() => envSchema.parse(validDataWithExtraKey)).toThrow();
+        });
+    });
+});
---
+++ backend/src/__tests__/schemas/transaction.spec.ts
@@ -0,0 +1,88 @@
+import { ObjectId } from 'mongodb';
+import {
+    transactionCreateSchema,
+    transactionSchema,
+    transactionUpdateSchema,
+} from '../../schemas/transaction';
+
+describe('transaction schemas', () => {
+    describe('transactionSchema', () => {
+        const validData = {
+            _id: new ObjectId(),
+            month: '2025-10',
+            name: 'Salary',
+            value: 1200.5,
+            isIncome: true,
+            isRecurring: false,
+            userId: new ObjectId(),
+            createdAt: new Date(),
+            updatedAt: null,
+        };
+
+        const validDataWithExtraKey = {
+            ...validData,
+            extraKey: 'extra value',
+        };
+
+        it('should accept valid data', () => {
+            const data = transactionSchema.parse(validData);
+            expect(data).toEqual(validData);
+        });
+
+        it('should refuse extra key', () => {
+            expect(() => transactionSchema.parse(validDataWithExtraKey))
+                .toThrow;
+        });
+    });
+
+    describe('transactionCreateSchema', () => {
+        const validData = {
+            month: '2025-10',
+            name: 'Salary',
+            value: 1200.5,
+            isIncome: true,
+            isRecurring: false,
+        };
+
+        const validDataWithExtraKey = {
+            ...validData,
+            extraKey: 'extra value',
+        };
+
+        it('should accept valid data', () => {
+            const data = transactionCreateSchema.parse(validData);
+            expect(data).toEqual(validData);
+        });
+
+        it('should refuse extra key', () => {
+            expect(() =>
+                transactionCreateSchema.parse(validDataWithExtraKey),
+            ).toThrow();
+        });
+    });
+
+    describe('transactionUpdateSchema', () => {
+        const validData = {
+            name: 'Salary',
+            value: 1200.5,
+            isIncome: true,
+            isRecurring: false,
+        };
+
+        const validDataWithExtraKey = {
+            ...validData,
+            extraKey: 'extra value',
+        };
+
+        it('should accept valid data', () => {
+            const data = transactionUpdateSchema.parse(validData);
+            expect(data).toEqual(validData);
+        });
+
+        it('should refuse extra key', () => {
+            expect(() =>
+                transactionUpdateSchema.parse(validDataWithExtraKey),
+            ).toThrow();
+        });
+    });
+});
