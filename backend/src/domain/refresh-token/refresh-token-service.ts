bb343e41ccede83694b07f91058edd6850e146ab
Antoine-NVE <antoinenavette@gmail.com>
2025-11-02 15:33:34 +0100

    feat: check for duplicate tokens

--- backend/src/domain/refresh-token/refresh-token-service.ts
+++ backend/src/domain/refresh-token/refresh-token-service.ts
@@ -1,7 +1,8 @@
-import { ObjectId } from 'mongodb';
+import { MongoServerError, ObjectId } from 'mongodb';
 import { RefreshTokenRepository } from './refresh-token-repository';
 import { RefreshToken } from './refresh-token-types';
 import { NotFoundError } from '../../infrastructure/errors/not-found-error';
+import { InternalServerError } from '../../infrastructure/errors/internal-server-error';
 
 export class RefreshTokenService {
     private readonly TTL = 7 * 24 * 60 * 60 * 1000; // 7 days
@@ -18,7 +19,14 @@ export class RefreshTokenService {
             updatedAt: null,
         };
 
-        await this.refreshTokenRepository.insertOne(refreshToken);
+        await this.refreshTokenRepository
+            .insertOne(refreshToken)
+            .catch((err) => {
+                if (err instanceof MongoServerError && err.code === 11000) {
+                    throw new InternalServerError('Duplicate token');
+                }
+                throw err;
+            });
 
         return refreshToken;
     };
