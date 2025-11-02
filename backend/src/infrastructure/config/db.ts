91ed1221eb52a4a32a57d984f8f243177d11fd56
Antoine-NVE <antoinenavette@gmail.com>
2025-11-02 15:30:47 +0100

    feat: add index on transactions

--- backend/src/infrastructure/config/db.ts
+++ backend/src/infrastructure/config/db.ts
@@ -7,12 +7,15 @@ const client = new MongoClient(`mongodb://database:27017`);
     const db = client.db('ledge');
 
     await db.collection('users').createIndex({ email: 1 }, { unique: true });
+
     await db
         .collection('refreshtokens')
         .createIndex({ token: 1 }, { unique: true });
     await db
         .collection('refreshtokens')
         .createIndex({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+    await db.collection('transactions').createIndex({ userId: 1 });
 })();
 
 export const db = client.db('ledge');
