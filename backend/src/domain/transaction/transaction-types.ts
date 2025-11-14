63806089935bc15d4c3b258e7c8002013e309538
Antoine-NVE <antoinenavette@gmail.com>
2025-11-14 17:25:24 +0100

    refactor: update transaction types

--- backend/src/domain/transaction/transaction-types.ts
+++ backend/src/domain/transaction/transaction-types.ts
@@ -1,17 +1,27 @@
 import { ObjectId } from 'mongodb';
 import { BaseDocument } from '../shared/shared-types';
 
-export type TransactionData = {
+type Income = {
     month: string;
     name: string;
     value: number;
-    type: 'income' | 'expense';
+    type: 'income';
     userId: ObjectId;
 };
 
+type Expense = {
+    month: string;
+    name: string;
+    value: number;
+    type: 'expense';
+    expenseCategory: 'need' | 'want' | 'investment' | null;
+    userId: ObjectId;
+};
+
+export type TransactionData = Income | Expense;
+
 export type Transaction = TransactionData & BaseDocument;
 
-export type UpdateTransactionData = Pick<
-    TransactionData,
-    'name' | 'value' | 'type'
->;
+export type UpdateTransactionData =
+    | Pick<Income, 'name' | 'value' | 'type'>
+    | Pick<Expense, 'name' | 'value' | 'type' | 'expenseCategory'>;
