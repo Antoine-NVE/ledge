f8d5a98a5ca155ce7bfde4ec40065c21b8cf2981
Antoine-NVE <antoinenavette@gmail.com>
2025-11-16 16:16:11 +0100

    refactor: create cache config

---
+++ backend/src/infrastructure/config/cache-config.ts
@@ -0,0 +1,15 @@
+import { createClient, RedisClientType } from 'redis';
+
+export const client: RedisClientType = createClient({
+    url: 'redis://cache:6379',
+});
+
+(async () => {
+    try {
+        await client.connect();
+        console.log('Redis connected');
+    } catch (err) {
+        console.error('Redis connection failed:', err);
+        process.exit(1);
+    }
+})();
