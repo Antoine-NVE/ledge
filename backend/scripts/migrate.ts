302a9e24c17e37bad8699c136fdabefc3054207b
Antoine-NVE <antoinenavette@gmail.com>
2025-11-23 16:51:54 +0100

    refactor: update migration script

--- backend/scripts/migrate.ts
+++ backend/scripts/migrate.ts
@@ -1,30 +1,69 @@
 import { MongoDBStorage, Umzug } from 'umzug';
-import { client, db } from '../src/infrastructure/config/db-config';
 import path from 'node:path';
+import { Logger } from 'pino';
+import { createLogger } from '../src/infrastructure/config/logger-config';
+import { connectToDb } from '../src/infrastructure/config/db-config';
+import { MongoClient } from 'mongodb';
 
-const umzug = new Umzug({
-    migrations: {
-        glob: path.join(__dirname, 'migrations', `*.js`),
-    },
-    context: db,
-    storage: new MongoDBStorage({
-        connection: db,
-    }),
-    logger: console,
-});
+const start = async () => {
+    const logger = createLogger(
+        process.env.NODE_ENV === 'development' ? 'development' : 'production',
+    );
 
-const direction = process.argv[2];
+    const { db, client } = await step(
+        'MongoDB connection',
+        logger,
+        async () => {
+            const { db, client } = await connectToDb();
+            logger.info('MongoDB connected');
+            return { db, client };
+        },
+    );
 
-(async () => {
-    try {
+    const umzug = new Umzug({
+        migrations: {
+            glob: path.join(__dirname, 'migrations', `*.js`),
+        },
+        context: db,
+        storage: new MongoDBStorage({
+            connection: db,
+        }),
+        logger: undefined,
+    });
+    umzug.on('migrating', (migration) => {
+        logger.info({ migrationName: migration.name }, 'Migration started');
+    });
+    umzug.on('migrated', (migration) => {
+        logger.info({ migrationName: migration.name }, 'Migration finished');
+    });
+
+    await step('Migration', logger, async () => {
+        const direction = process.argv[2];
         if (direction === 'up') await umzug.up();
         if (direction === 'down') await umzug.down();
+        logger.info('Migrations done');
+    });
 
-        console.log('Migration done');
+    return client;
+};
+
+const step = async <T>(
+    name: string,
+    logger: Logger,
+    fn: () => Promise<T>,
+): Promise<T> => {
+    try {
+        return await fn();
     } catch (err) {
-        console.error('Migration failed:', err);
-        process.exit(1);
-    } finally {
-        await client.close();
+        return stop(logger, name, err); // Return nothing
     }
-})();
+};
+
+const stop = (logger: Logger, stepName: string, err: unknown) => {
+    logger.fatal({ err }, `${stepName} failed`);
+    process.exit(1);
+};
+
+start().then(async (client: MongoClient) => {
+    await client.close();
+});
