4dd575f26d41dde6df885d727e4d3532bd4121be
Antoine-NVE <antoinenavette@gmail.com>
2025-12-04 19:13:27 +0100

    refactor: make all migrations idempotent

--- backend/scripts/migrations/005-remove-email-verification-cooldown-expires-at.ts
+++ backend/scripts/migrations/005-remove-email-verification-cooldown-expires-at.ts
@@ -3,7 +3,10 @@ import { Db } from 'mongodb';
 export const up = async ({ context: db }: { context: Db }) => {
     await db
         .collection('users')
-        .updateMany({}, { $unset: { emailVerificationCooldownExpiresAt: '' } });
+        .updateMany(
+            { emailVerificationCooldownExpiresAt: { $exists: true } },
+            { $unset: { emailVerificationCooldownExpiresAt: '' } },
+        );
 };
 
 export const down = async () => {};
