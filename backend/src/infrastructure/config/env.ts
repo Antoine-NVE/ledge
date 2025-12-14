d23d3c2abbde51f5e9ca11fc81780680a08bef01
Antoine-NVE <antoinenavette@gmail.com>
2025-12-14 18:45:21 +0100

    refactor: simplify env

--- backend/src/infrastructure/config/env.ts
+++ backend/src/infrastructure/config/env.ts
@@ -2,36 +2,25 @@ import z from 'zod';
 import { parseArray, parseNumber } from '../../core/utils/parse';
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['development', 'production']),
-    TOKEN_SECRET: z.string(),
-    ALLOWED_ORIGINS: z.array(z.url()),
-    PORT: z.number().optional(),
-    DATABASE_URL: z.url(),
-    CACHE_URL: z.url(),
-    SMTP_URL: z.url(),
-    EMAIL_FROM: z.string(),
+    nodeEnv: z.enum(['development', 'production']),
+    tokenSecret: z.string(),
+    allowedOrigins: z.array(z.url()),
+    port: z.number().optional(),
+    databaseUrl: z.url(),
+    cacheUrl: z.url(),
+    smtpUrl: z.url(),
+    emailFrom: z.string(),
 });
 
 export const loadEnv = () => {
-    const env = envSchema.parse({
-        NODE_ENV: process.env.NODE_ENV,
-        TOKEN_SECRET: process.env.TOKEN_SECRET,
-        ALLOWED_ORIGINS: parseArray(process.env.ALLOWED_ORIGINS),
-        PORT: parseNumber(process.env.PORT),
-        DATABASE_URL: process.env.DATABASE_URL,
-        CACHE_URL: process.env.CACHE_URL,
-        SMTP_URL: process.env.SMTP_URL,
-        EMAIL_FROM: process.env.EMAIL_FROM,
+    return envSchema.parse({
+        nodeEnv: process.env.NODE_ENV,
+        tokenSecret: process.env.TOKEN_SECRET,
+        allowedOrigins: parseArray(process.env.ALLOWED_ORIGINS),
+        port: parseNumber(process.env.PORT),
+        databaseUrl: process.env.DATABASE_URL,
+        cacheUrl: process.env.CACHE_URL,
+        smtpUrl: process.env.SMTP_URL,
+        emailFrom: process.env.EMAIL_FROM,
     });
-
-    return {
-        nodeEnv: env.NODE_ENV,
-        tokenSecret: env.TOKEN_SECRET,
-        allowedOrigins: env.ALLOWED_ORIGINS,
-        port: env.PORT,
-        databaseUrl: env.DATABASE_URL,
-        cacheUrl: env.CACHE_URL,
-        smtpUrl: env.SMTP_URL,
-        emailFrom: env.EMAIL_FROM,
-    };
 };
