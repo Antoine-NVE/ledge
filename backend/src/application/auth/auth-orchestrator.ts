d1ce4eba258916bd808c0e51bba229b5c7de82f2
Antoine-NVE <antoinenavette@gmail.com>
2025-12-26 12:01:27 +0100

    refactor: fix auth orchestrator

--- backend/src/application/auth/auth-orchestrator.ts
+++ backend/src/application/auth/auth-orchestrator.ts
@@ -10,6 +10,7 @@ import { RefreshTokenRepository } from '../../domain/refresh-token/refresh-token
 import { ConflictError } from '../../core/errors/conflict-error';
 import { Result } from '../../core/types/result';
 import { fail, ok } from '../../core/utils/result';
+import { IdGenerator } from '../ports/id-generator';
 
 type RegisterInput = {
     email: string;
@@ -48,7 +49,7 @@ type LogoutInput = {
 
 type LogoutOutput = {
     refreshToken: RefreshToken;
-};
+} | void;
 
 export class AuthOrchestrator {
     private readonly REFRESH_TOKEN_DURATION = 7 * 24 * 60 * 60 * 1000;
@@ -58,102 +59,108 @@ export class AuthOrchestrator {
         private tokenManager: TokenManager,
         private refreshTokenRepository: RefreshTokenRepository,
         private hasher: Hasher,
+        private idGenerator: IdGenerator,
     ) {}
 
     register = async ({ email, password }: RegisterInput): Promise<Result<RegisterOutput, ConflictError | Error>> => {
         const now = new Date();
 
-        const passwordHash = await this.hasher.hash(password);
-
-        const userResult = await this.userRepository.create({
+        const user: User = {
+            id: this.idGenerator.generate(),
             email,
-            passwordHash,
+            passwordHash: await this.hasher.hash(password),
             isEmailVerified: false,
             createdAt: now,
-        });
+            updatedAt: now,
+        };
+        const userResult = await this.userRepository.create(user);
         if (!userResult.success) return fail(userResult.error);
-        const user = userResult.value;
 
-        const refreshTokenResult = await this.refreshTokenRepository.create({
+        const refreshToken: RefreshToken = {
+            id: this.idGenerator.generate(),
             userId: user.id,
-            token: generateToken(),
+            value: generateToken(),
             expiresAt: new Date(now.getTime() + this.REFRESH_TOKEN_DURATION),
             createdAt: now,
-        });
+            updatedAt: now,
+        };
+        const refreshTokenResult = await this.refreshTokenRepository.create(refreshToken);
         if (!refreshTokenResult.success) return fail(refreshTokenResult.error);
-        const refreshToken = refreshTokenResult.value;
 
         const accessToken = this.tokenManager.signAccess({ userId: user.id });
 
         return ok({ user, accessToken, refreshToken });
     };
 
-    login = async ({ email, password }: LoginInput): Promise<Result<LoginOutput, UnauthorizedError | Error>> => {
+    login = async ({ email, password }: LoginInput): Promise<Result<LoginOutput, Error | UnauthorizedError>> => {
         const now = new Date();
 
         const userResult = await this.userRepository.findByEmail(email);
         if (!userResult.success) {
             const err = userResult.error;
-            if (err instanceof NotFoundError) {
-                return fail(new UnauthorizedError({ message: 'Invalid credentials' }));
-            }
+            if (err instanceof NotFoundError) return fail(new UnauthorizedError({ message: 'Invalid credentials' }));
             return fail(err);
         }
         const user = userResult.value;
 
-        const doesMatch = await this.hasher.compare(password, user.passwordHash);
-        if (!doesMatch) {
-            return fail(new UnauthorizedError({ message: 'Invalid credentials' }));
-        }
+        const isValidPassword = await this.hasher.compare(password, user.passwordHash);
+        if (!isValidPassword) return fail(new UnauthorizedError({ message: 'Invalid credentials' }));
 
-        const refreshTokenResult = await this.refreshTokenRepository.create({
+        const refreshToken: RefreshToken = {
+            id: this.idGenerator.generate(),
             userId: user.id,
-            token: generateToken(),
+            value: generateToken(),
             expiresAt: new Date(now.getTime() + this.REFRESH_TOKEN_DURATION),
             createdAt: now,
-        });
+            updatedAt: now,
+        };
+        const refreshTokenResult = await this.refreshTokenRepository.create(refreshToken);
         if (!refreshTokenResult.success) return fail(refreshTokenResult.error);
-        const refreshToken = refreshTokenResult.value;
 
         const accessToken = this.tokenManager.signAccess({ userId: user.id });
 
         return ok({ user, accessToken, refreshToken });
     };
 
-    refresh = async ({ token }: RefreshInput): Promise<Result<RefreshOutput, UnauthorizedError | Error>> => {
+    refresh = async ({ token }: RefreshInput): Promise<Result<RefreshOutput, Error | UnauthorizedError>> => {
         const now = new Date();
 
-        const findResult = await this.refreshTokenRepository.findByToken(token);
+        const findResult = await this.refreshTokenRepository.findByValue(token);
         if (!findResult.success) {
             const err = findResult.error;
-            if (err instanceof NotFoundError) {
-                return fail(new UnauthorizedError({ message: 'Invalid refresh token' }));
-            }
+            if (err instanceof NotFoundError) return fail(new UnauthorizedError({ message: 'Invalid refresh token' }));
             return fail(err);
         }
         const refreshToken = findResult.value;
 
-        if (refreshToken.expiresAt < now) {
-            return fail(new UnauthorizedError({ message: 'Expired refresh token' }));
-        }
+        if (refreshToken.expiresAt < now) return fail(new UnauthorizedError({ message: 'Expired refresh token' }));
 
-        refreshToken.token = generateToken();
+        refreshToken.value = generateToken();
         refreshToken.expiresAt = new Date(now.getTime() + this.REFRESH_TOKEN_DURATION);
         refreshToken.updatedAt = now;
         const saveResult = await this.refreshTokenRepository.save(refreshToken);
         if (!saveResult.success) return fail(saveResult.error);
 
-        const accessToken = this.tokenManager.signAccess({
-            userId: refreshToken.userId,
-        });
+        const accessToken = this.tokenManager.signAccess({ userId: refreshToken.userId });
 
         return ok({ accessToken, refreshToken });
     };
 
-    logout = async ({ token }: LogoutInput): Promise<Result<LogoutOutput, NotFoundError | Error>> => {
-        const result = await this.refreshTokenRepository.deleteByToken(token);
-        if (!result.success) return fail(result.error);
-        const refreshToken = result.value;
+    logout = async ({ token }: LogoutInput): Promise<Result<LogoutOutput, Error | NotFoundError>> => {
+        const findResult = await this.refreshTokenRepository.findByValue(token);
+        if (!findResult.success) {
+            const err = findResult.error;
+            if (err instanceof NotFoundError) return ok(undefined);
+            return fail(err);
+        }
+        const refreshToken = findResult.value;
+
+        const deleteResult = await this.refreshTokenRepository.delete(refreshToken);
+        if (!deleteResult.success) {
+            const err = deleteResult.error;
+            if (err instanceof NotFoundError) return ok(undefined);
+            return fail(err);
+        }
 
         return ok({ refreshToken });
     };
