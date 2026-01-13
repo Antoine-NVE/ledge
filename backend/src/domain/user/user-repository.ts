ecc0f4ba724f236af152e04243d73461cfe4e766
Antoine-NVE <antoinenavette@gmail.com>
2026-01-13 21:05:14 +0100

    refactor: update user repository interface

--- backend/src/domain/user/user-repository.ts
+++ backend/src/domain/user/user-repository.ts
@@ -1,11 +1,8 @@
 import type { User } from './user-types.js';
-import { ConflictError } from '../../core/errors/conflict-error.js';
-import { NotFoundError } from '../../core/errors/not-found-error.js';
-import type { Result } from '../../core/types/result.js';
 
 export interface UserRepository {
-    create: (user: User) => Promise<Result<void, ConflictError | Error>>;
-    findById: (id: string) => Promise<Result<User | null, Error>>;
-    findByEmail: (email: string) => Promise<Result<User | null, Error>>;
-    save: (user: User) => Promise<Result<void, Error | NotFoundError>>;
+    create: (user: User) => Promise<void>;
+    findById: (id: string) => Promise<User | null>;
+    findByEmail: (email: string) => Promise<User | null>;
+    save: (user: User) => Promise<void>;
 }
