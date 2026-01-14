b08556d563fdfd81a2946eb2ba7eca2fd53d4422
Antoine-NVE <antoinenavette@gmail.com>
2026-01-14 19:24:42 +0100

    refactor: update refresh token repository interface

--- backend/src/domain/refresh-token/refresh-token-repository.ts
+++ backend/src/domain/refresh-token/refresh-token-repository.ts
@@ -1,10 +1,8 @@
 import type { RefreshToken } from './refresh-token-types.js';
-import { NotFoundError } from '../../core/errors/not-found-error.js';
-import type { Result } from '../../core/types/result.js';
 
 export interface RefreshTokenRepository {
-    create(refreshToken: RefreshToken): Promise<Result<void, Error>>;
-    findByValue(value: string): Promise<Result<RefreshToken | null, Error>>;
-    save(refreshToken: RefreshToken): Promise<Result<void, Error | NotFoundError>>;
-    delete(refreshToken: RefreshToken): Promise<Result<void, Error | NotFoundError>>;
+    create(refreshToken: RefreshToken): Promise<void>;
+    findByValue(value: string): Promise<RefreshToken | null>;
+    save(refreshToken: RefreshToken): Promise<void>;
+    delete(refreshToken: RefreshToken): Promise<void>;
 }
