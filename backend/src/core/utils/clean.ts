47555ad16ae1774e0e76fb944c28a44387addd2f
Antoine-NVE <antoinenavette@gmail.com>
2026-01-20 19:11:00 +0100

    refactor: remove old function

--- backend/src/core/utils/clean.ts
+++
@@ -1,8 +0,0 @@
-import type { User } from '../../domain/entities/user.js';
-
-export const removePasswordHash = (user: User): Omit<User, 'passwordHash'> => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { passwordHash, ...safeUser } = user;
-
-    return safeUser;
-};
