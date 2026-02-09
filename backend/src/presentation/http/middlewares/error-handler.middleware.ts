908e466d4c4777790a52fe3953c1024ca0321409
Antoine-NVE <antoinenavette@gmail.com>
2026-02-09 12:19:26 +0100

    refactor: add http errors to error handler

--- backend/src/presentation/http/middlewares/error-handler.middleware.ts
+++ backend/src/presentation/http/middlewares/error-handler.middleware.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from 'express';
 import type { ApiError } from '@shared/api/api-response.js';
+import { HttpError } from '../errors/http.error.js';
 
 export const errorHandlerMiddleware = () => {
     return (
@@ -8,6 +9,12 @@ export const errorHandlerMiddleware = () => {
         res: Response,
         _next: NextFunction, // eslint-disable-line @typescript-eslint/no-unused-vars
     ): void => {
+        if (err instanceof HttpError) {
+            req.logger.warn(err.message, { err });
+            res.status(err.status).json(err.body);
+            return;
+        }
+
         req.logger.error(err instanceof Error ? err.message : 'Unknown error', { err });
         const response: ApiError = {
             success: false,
