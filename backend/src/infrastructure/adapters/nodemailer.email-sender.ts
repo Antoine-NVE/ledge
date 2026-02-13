3b1a6a56e578f774976a9edd6d8807c7bd7900f0
Antoine-NVE <antoinenavette@gmail.com>
2026-02-13 15:37:34 +0100

    refactor: simplify nodemailer email sender

--- backend/src/infrastructure/adapters/nodemailer.email-sender.ts
+++ backend/src/infrastructure/adapters/nodemailer.email-sender.ts
@@ -4,10 +4,6 @@ import type { EmailSender } from '../../domain/ports/email-sender.js';
 export class NodemailerEmailSender implements EmailSender {
     constructor(private transporter: Transporter) {}
 
-    private send = async (options: { from: string; to: string; subject: string; html: string }): Promise<void> => {
-        await this.transporter.sendMail(options);
-    };
-
     sendEmailVerification = async ({
         from,
         to,
@@ -22,6 +18,6 @@ export class NodemailerEmailSender implements EmailSender {
         const subject = 'Please verify your email address';
         const html = `Click here to verify your email address: <a href="${frontendBaseUrl}/verify-email/${emailVerificationToken}">verify email</a>. This link will expire in 1 hour.`;
 
-        await this.send({ from, to, subject, html });
+        await this.transporter.sendMail({ from, to, subject, html });
     };
 }
