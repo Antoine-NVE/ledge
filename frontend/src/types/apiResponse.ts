9818c87899aa817f6246214d86547ab40b1742c4
Antoine-NVE <antoinenavette@gmail.com>
2026-02-15 10:09:02 +0100

    refactor(frontend): remove type files

--- frontend/src/types/apiResponse.ts
+++
@@ -1,5 +0,0 @@
-export interface ApiResponse<T, E = Record<string, string>> {
-    message: string; // A message describing the result of the API call, e.g., "Success" or "Error"
-    data: T; // Data can be null if there's no data to return
-    fields: E; // Fields can be null if there are no errors
-}
