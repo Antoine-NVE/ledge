cccb6a05f6dd3606a1532dd380d177936da8ec4c
Antoine-NVE <antoinenavette@gmail.com>
2026-02-19 07:37:05 +0100

    refactor: clean profile page

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,47 +1,54 @@
 import { useState } from 'react';
-import { sendVerificationEmail } from '../api/users.ts';
-import useUser from '../hooks/useUser';
+import { useAuth } from '../contexts/AuthContext.tsx';
+import { requestEmailVerification } from '../api/users.ts';
+import { Navigate } from 'react-router-dom';
+import Navbar from '../components/Navbar.tsx';
 
 const Profile = () => {
-    const { user } = useUser();
-
-    const [isLoading, setIsLoading] = useState(false);
+    const [isSending, setIsSending] = useState(false);
     const [success, setSuccess] = useState<boolean | null>(null);
     const [message, setMessage] = useState<string | null>(null);
 
-    const handleSendVerificationEmail = async () => {
-        setMessage(null);
+    const { user, isLoading } = useAuth();
+    if (isLoading) return <div className="flex flex-col flex-1 items-center justify-center p-4">Loading...</div>;
+    if (!user) return <Navigate to="/login" />;
+
+    const handleRequestEmailVerification = async () => {
         setSuccess(null);
+        setMessage(null);
 
-        setIsLoading(true);
-        const [result, response] = await sendVerificationEmail();
-        setIsLoading(false);
+        setIsSending(true);
+        const response = await requestEmailVerification({ frontendBaseUrl: window.location.origin });
+        setIsSending(false);
 
-        setMessage(result.message);
-
-        if (!response || !response.ok) {
+        if (!response.success) {
             setSuccess(false);
+            setMessage(response.code);
             return;
         }
 
         setSuccess(true);
+        setMessage('Email sent successfully!');
     };
 
     return (
-        <div className="flex flex-col flex-1 items-center justify-center p-4">
-            <h1 className="text-4xl font-bold mb-4">Profile page</h1>
-            <p className="text-lg mb-6">This is the profile page.</p>
-            {!user?.isEmailVerified && (
-                <button
-                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition cursor-pointer disabled:opacity-50"
-                    onClick={handleSendVerificationEmail}
-                    disabled={isLoading}
-                >
-                    {isLoading ? 'Sending...' : 'Send verification email'}
-                </button>
-            )}
-            {message && <div className={`mt-4 ${success ? 'text-green-600' : 'text-red-600'}`}>{message}</div>}
-        </div>
+        <>
+            <Navbar />
+            <div className="flex flex-col flex-1 items-center justify-center p-4">
+                <h1 className="text-4xl font-bold mb-4">Profile page</h1>
+                <p className="text-lg mb-6">This is the profile page.</p>
+                {!user.isEmailVerified && (
+                    <button
+                        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition cursor-pointer disabled:opacity-50"
+                        onClick={handleRequestEmailVerification}
+                        disabled={isSending}
+                    >
+                        {isSending ? 'Sending...' : 'Request email verification'}
+                    </button>
+                )}
+                {message && <div className={`mt-4 ${success ? 'text-green-600' : 'text-red-600'}`}>{message}</div>}
+            </div>
+        </>
     );
 };
 
